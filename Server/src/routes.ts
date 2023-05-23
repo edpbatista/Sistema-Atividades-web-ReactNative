d6909353d53b44b961e7d62e485f48ecb11cb39f
Edimar Pereira <edpbatista2@gmail.com>
2023-05-23 09:44:27 -0400

    Preparação de routes para receber as requisição de Get do Controller

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -1,25 +1,35 @@
 import { Router } from "express";
-import { CreateAtividades } from "./controllers/createAtividadeController";
-import { DeleteAtividade } from "./controllers/DeleteAtividadeController";
-import { UpdateAtividade } from "./controllers/UpdateAtividadeController";
-import { CreateUsuario } from "./controllers/createUsuarioController";
+import { CreateAtividades } from "../src/controllers/atividades/createAtividadeController";
+import { DeleteAtividade } from "../src/controllers/atividades/DeleteAtividadeController";
+import { UpdateAtividade } from "../src/controllers/atividades/UpdateAtividadeController";
+import { GetAtividade } from "../src/controllers/atividades/GetAtividadeController";
+
+import { CreateUsuario } from "../src/controllers/usuarios/createUsuarioController";
+import { DeleteUsuario } from "../src/controllers/usuarios/DeleteUsuarioController";
+import { UpdateUsuario } from "../src/controllers/usuarios/UpdateUsuarioController";
+import { GetUsuarios } from "../src/controllers/usuarios/GetUsuarioController";
 
 const router = Router();
 
 const createAtividades = new CreateAtividades();
 const deleteAtividade = new DeleteAtividade();
 const updateAtividade = new UpdateAtividade();
+const getAtividade = new GetAtividade();
 
 const createUsuario = new CreateUsuario();
-const deleteUsuario = new DeleteAtividade();
-const updateUsuario = new UpdateAtividade();
+const deleteUsuario = new DeleteUsuario();
+const updateUsuario = new UpdateUsuario();
+const getUsuarios = new GetUsuarios();
 
 router.post("/atividade", createAtividades.handle);
 router.delete("/atividade/:id", deleteAtividade.handle);
 router.put("/atividade/:id", updateAtividade.handle);
+router.get("/atividade", getAtividade.handle);
 
 router.post("/usuario", createUsuario.handle);
 router.delete("/usuario/:id", deleteUsuario.handle);
 router.put("/usuario/:id", updateUsuario.handle);
+router.get("/usuarios", getUsuarios.handle);
+
 
 export { router };
